e92439e57b4b028148b5cde93c805f357e5715c6
Mickhat <root@mickhat.xyz>
2025-03-16 19:43:31 +0100

    added docker and burpsuite

--- configuration.nix
+++ configuration.nix
@@ -48,6 +48,8 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  virtualisation.docker.enable = true;
+
   # Enable the XFCE Desktop Environment.
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
