6f445eb77b314b13f760f3837a08b1ff6a15f1e9
Mickhat <root@mickhat.xyz>
2025-03-19 08:31:26 +0100

    revert

--- configuration.nix
+++ configuration.nix
@@ -127,10 +127,6 @@
   };
 
   virtualisation.docker.enable = true;
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
-  virtualisation.virtualbox.guest.enable = true;
-  virtualisation.virtualbox.guest.dragAndDrop = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.mickhat = {
